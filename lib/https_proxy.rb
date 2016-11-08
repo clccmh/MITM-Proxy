f4cfd2608cabb1628a01308f4fef7554c2465a9b
Carter Hay <clccmh@gmail.com>
2016-11-07 22:27:15 -0600

    continued working on https

--- lib/https_proxy.rb
+++ lib/https_proxy.rb
@@ -6,18 +6,36 @@ require 'webrick'
 require 'webrick/httpproxy'
 require 'zlib'
 
+class WEBrick::HTTPRequest
+
+  attr_accessor :header, :request_method, :unparsed_uri, :request_uri
+
+  def set_request_method(method)
+    @request_method = method
+  end
+
+  def set_unparsed_uri(uri)
+    @unparsed_uri = uri
+  end
+  
+end
+
 class WEBrick::HTTPProxyServer
 
   def service(req, res)
     if req.request_method == "CONNECT"
       #do_CONNECT(req, res)
-      puts 'HTTPS request!!'
-      puts req.accept
-      puts req.request_method
-      puts req.unparsed_uri
-      puts req.path
-      puts req.query_string
-      puts req.request_uri
+      #puts req.accept
+      #puts req.request_method
+      #puts req.unparsed_uri
+      #puts req.path
+      #puts req.query_string
+      #puts req.request_uri
+      #puts '------------------------------------'
+      req.request_method = 'GET'
+      req.unparsed_uri = 'carterhay.net:80'
+      puts req.header[:host][0]
+
     elsif req.unparsed_uri =~ %r!^http://!
       proxy_service(req, res)
     else
@@ -30,7 +48,7 @@ end
 
 proxy = WEBrick::HTTPProxyServer.new(
   Port: 8000
-  )
+)
 
 
 proxy.start
